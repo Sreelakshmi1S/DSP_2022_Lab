c816059186eb4d4ba9ec6d14b64e492fe83db4a3
Sreelakshmi S <sreelakshmiponakam@gmail.com>
2022-04-17 17:24:29 +0530

    Create Qn_1.py

---
+++ Qn_1.py
@@ -0,0 +1,5 @@
+"""1) Implement convolution of two sequences of length K and M (K ≥ M) using python. (Do not use
+ available library function for convolution.). Use the program to compute [1 2 3] ∗ [1 1 1 1]."""
+   
+  
+ 
