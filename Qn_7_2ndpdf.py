8def4b54a1bcbd6fbabf25fafc0b718d22d72966
Sreelakshmi S <sreelakshmiponakam@gmail.com>
2022-04-21 14:08:35 +0530

    Create Qn_7_2ndpdf.py

---
+++ Qn_7_2ndpdf.py
@@ -0,0 +1,6 @@
+"""Write a python function to compute discrete Fourier transform (DFT) and inverse DFT of an N-point
+signal. Verify the property
+
+DFT[x1[n]x2[n]] = 1/N{X1[k]*X2[k]}
+
+with the help of a python program. Use x1[n] = {−1, 1, 1, −1} and x1[n] = {1, −1, 1, −1}"""
